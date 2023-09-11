105409de04ae444c7270909a4d3022146e6b53d8
nmy6452 <nmy6452@naver.com>
2023-09-12 00:03:04 +0900

    FEAT: 리액트 임포트

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+function App() {
+   const [hello, setHello] = useState('')
+
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => setHello(response.data))
+        .catch(error => console.log(error))
+    }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          여기에 넣어 : {hello}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
